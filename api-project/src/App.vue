4070db254fecc727f2d384cfef2db90639c6fcf2
SamN4 <samngsit@gmail.com>
2022-03-21 10:29:40 -0400

    commit

--- api-project/src/App.vue
+++ api-project/src/App.vue
@@ -1,20 +1,14 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="flex-container">
+      <button><router-link to="/">Home</router-link></button>
+      <button><router-link to="/about">About</router-link></button>
+    </div>
   </nav>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
 nav {
   padding: 30px;
 }
@@ -26,5 +20,9 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.flex-container{
+  
 }
 </style>
